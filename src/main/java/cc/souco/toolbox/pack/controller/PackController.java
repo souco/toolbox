e0b022b5eef11231a300c3ac7bba47babc0c5bdb
souco <souco@qq.com>
2019-02-25 00:13:57 +0800

    项目打包页面逻辑完善

--- src/main/java/cc/souco/toolbox/pack/controller/PackController.java
+++ src/main/java/cc/souco/toolbox/pack/controller/PackController.java
@@ -1,14 +1,39 @@
 package cc.souco.toolbox.pack.controller;
 
+import cc.souco.toolbox.common.Ret;
+import cc.souco.toolbox.pack.service.SvnService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/pack")
 public class PackController {
 
+    @Autowired
+    private SvnService svnService;
+
     @RequestMapping("")
     public String pack(){
         return "pack/index";
+    }
+
+    @ResponseBody
+    @RequestMapping("/testSvnConfig")
+    public Ret testSvnConfig(String username, String password, String location){
+        Ret ret = Ret.ok();
+        try {
+            boolean result = svnService.testSvnConfig(username, password, location);
+            if (result) {
+                ret.set("msg", "测试成功");
+            } else {
+                return ret.setFail("用户名或密码不正确！");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ret.setFail(e.getMessage());
+        }
+        return ret;
     }
 }
--- src/main/java/cc/souco/toolbox/pack/service/SvnService.java
+++ src/main/java/cc/souco/toolbox/pack/service/SvnService.java
@@ -2,6 +2,8 @@ package cc.souco.toolbox.pack.service;
 
 import cc.souco.toolbox.pack.UseSvnKit;
 import com.beust.jcommander.internal.Lists;
+import org.springframework.stereotype.Service;
+import org.tmatesoft.svn.core.SVNAuthenticationException;
 import org.tmatesoft.svn.core.SVNException;
 import org.tmatesoft.svn.core.SVNLogEntry;
 import org.tmatesoft.svn.core.auth.BasicAuthenticationManager;
@@ -15,7 +17,36 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
+@Service
 public class SvnService {
+
+    /**
+     * 测试svn账户信息是否正确
+     * @param username svn用户名
+     * @param password svn密码
+     * @param location svn项目路径
+     * @return 是否测试通过
+     */
+    public boolean testSvnConfig(String username, String password, String location) {
+        try {
+            SVNLogClient logClient = getSvnLogClient(username, password);
+            File[] files = {new File(location)};
+            SVNRevision end = SVNRevision.create(-1L);
+            logClient.doLog(files, null, end, true, true, 1, null);
+        } catch (SVNAuthenticationException authException) {
+            return false;
+        } catch (SVNException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    private SVNLogClient getSvnLogClient(String username, String password){
+        BasicAuthenticationManager authManager = BasicAuthenticationManager.newInstance(username, password.toCharArray());
+        SVNClientManager clientManager = SVNClientManager.newInstance(null, authManager);
+        return clientManager.getLogClient();
+    }
 
     private SVNLogClient getSvnLogClient(){
         BasicAuthenticationManager authManager = BasicAuthenticationManager.newInstance(UseSvnKit.SVN_NAME, UseSvnKit.SVN_PASSWORD.toCharArray());
